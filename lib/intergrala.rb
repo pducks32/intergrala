86cfb8b0efdacb6e94eaa4d3b341e992914bfb38
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-02-07 18:58:57 -0600

    Added Ajax Functionality

--- lib/intergrala.rb
+++ lib/intergrala.rb
@@ -1,12 +1,21 @@
 require "sinatra/base"
+require 'json'
 
 module Intergrala
 
   class Engine < Sinatra::Base
+
+    set :public_folder, 'dist'
+
+    get '/' do
+      send_file File.join(settings.public_folder, 'index.html')
+    end
+
     post '/equation' do
+      content_type :json
       request.body.rewind
-      data = JSON.parse request.body.read
-      data
+      data = JSON.parse(request.body.read)
+      data.to_json
     end
   end
 end
