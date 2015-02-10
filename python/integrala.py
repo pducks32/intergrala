bb1a31e2f6a5624c1767759d3ee6f50ec4aa4339
Ian Klatzco <iklatzco@gmail.com>
2015-02-09 18:56:59 -0600

    IT WORKSSSSSS

--- python/integrala.py
+++ python/integrala.py
@@ -14,9 +14,8 @@ import sys
 
 x = Symbol('x')
 y = Symbol('y')
-print(type(x))
 
-disk = 0 #where disk vs shell is drawn from the json
+disk = 0
 shell = 0
 volume = 0 #just defining for now
 
@@ -24,33 +23,24 @@ volume = 0 #just defining for now
 userInput = json.dumps({"dependentVariable":"x","independentVariable":"y","integrationMethod":"disk","expression":"y","upperBound":"1","lowerBound":"0"})
 #this turns it into a string
 userInput = json.loads(userInput)
-print(type(x))
-print(userInput["independentVariable"])
-print(type(userInput["independentVariable"]))
-sympify(userInput["independentVariable"]) #i expect this to convert it to a sympy symbol object, but it persists as unicode. whyyyyy I can't interact w it that way
-print(type(userInput["independentVariable"]))
-x.subs(x, userInput["independentVariable"])
-print(type(x))
-print(x)
-#bc oop/python
-#i honestly can't beelieve that worked
-#print(userInput["dependentVariable"]) #there we go, took long enough
+#above turns it into a python dict
 
-# userInput["expression"]
-#load from page (i have no actual idea how to take it from the page, but i should be able to match the json formatting)
+x = Symbol(userInput["dependentVariable"])
 
-# json.loads({"dependentVariable":"x","independentVariable":"y","integrationMethod":"disk","expression":"y","upperBound":"1","lowerBound":"0"}) #replace w wherever you get the json, for now has a dummy json
-
-# expr = x**2
+if userInput["integrationMethod"] == "disk":
+	disk = true
+elif userInput["integrationMethod"] == "shell":
+	print("Shell method is not yet implemented.")
 
 if (disk):
-	volume = pi * integrate((x),x)  #okay this works, that squares the function and treats it symbolically, then evals the integral.
+	volume = pi * integrate((x)**2,(x,0,1))  #okay this works, that squares the function and treats it symbolically, then evals the integral.
+	indefIntegral = pi * integrate((x)**2,x) #maybe you need this
 	#i'm using x here but this works with either x or y.
-	print(volume) #this should be shiny for passing it to ruby
+	pprint(volume) #need print as latex
 
 if (shell):
-	print()
+	volume = 2pi * integrate(x,x)
 	#volume = 2*pi * integrate(S((firstFunc)*(secondFunc)),x) #okay this is possibl the impossibly difficult part. defining the function won't be easy, i don't think, wo presetting it. 
 
 # i don't know if this is necessary, or if i can juts print it
-# print(json.dumps({"volume":volume})) #it needs to not be a print statement, but to feed to the page.
+# print(json.dumps({"volume":volume,"volumeExpression":volumeExpression}))
