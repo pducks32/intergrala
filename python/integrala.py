3763c3253ef374eb15336fdd96e4289b1747e808
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-02-10 07:59:53 -0600

    I'm too dame tired to name these commits

--- python/integrala.py
+++ python/integrala.py
@@ -1,6 +1,6 @@
 #python backend for integrala
 #takes JSON-formatted functions/x//y respects/disk//shell method and calculates an integral, then spits it back out as a JSON-formatted integral.
-#CURRENTLY FUNCTIONING: 
+#CURRENTLY FUNCTIONING:
 #Disk Method
 #TODO
 #shell method
@@ -28,18 +28,15 @@ x = Symbol(userInput["dependentVariable"])
 if userInput["integrationMethod"] == "disk":
 	disk = true
 elif userInput["integrationMethod"] == "shell":
-	print("Shell method is not yet implemented.")
+	# print("Shell method is not yet implemented.")
 
 if (disk):
 	volume = pi * integrate((x)**2,(x,0,1))  #okay this works, that squares the function and treats it symbolically, then evals the integral.
 	indefIntegral = pi * integrate((x)**2,x) #maybe you need this
-	#i'm using x here but this works with either x or y.
-	print(latex(volume)) #need print as latex
 
 if (shell):
 	volume = 2 * pi * integrate(x,x)
-	#volume = 2*pi * integrate(S((firstFunc)*(secondFunc)),x) #okay this is possibl the impossibly difficult part. defining the function won't be easy, i don't think, wo presetting it. 
-	print(latex(volume))
+	#volume = 2*pi * integrate(S((firstFunc)*(secondFunc)),x) #okay this is possibl the impossibly difficult part. defining the function won't be easy, i don't think, wo presetting it.
 
 # i don't know if this is necessary, or if i can juts print it
-# print(json.dumps({"volume":volume,"volumeExpression":volumeExpression}))
+print(json.dumps({"volume":volume,"latex":latex(volume)}))
