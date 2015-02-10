fe0e83afe95db86adcc17b2edfee1cbc3e5c5178
Ian Klatzco <iklatzco@gmail.com>
2015-02-09 20:22:40 -0600

    added latex

--- python/integrala.py
+++ python/integrala.py
@@ -19,8 +19,8 @@ disk = 0
 shell = 0
 volume = 0 #just defining for now
 
-# userInput = json.dumps(sys.argv)
-userInput = json.dumps({"dependentVariable":"x","independentVariable":"y","integrationMethod":"disk","expression":"y","upperBound":"1","lowerBound":"0"})
+userInput = json.dumps(sys.argv)
+# userInput = json.dumps({"dependentVariable":"x","independentVariable":"y","integrationMethod":"disk","expression":"y","upperBound":"1","lowerBound":"0"})
 #this turns it into a string
 userInput = json.loads(userInput)
 #above turns it into a python dict
@@ -36,11 +36,12 @@ if (disk):
 	volume = pi * integrate((x)**2,(x,0,1))  #okay this works, that squares the function and treats it symbolically, then evals the integral.
 	indefIntegral = pi * integrate((x)**2,x) #maybe you need this
 	#i'm using x here but this works with either x or y.
-	pprint(volume) #need print as latex
+	print(latex(volume)) #need print as latex
 
 if (shell):
-	volume = 2pi * integrate(x,x)
+	volume = 2 * pi * integrate(x,x)
 	#volume = 2*pi * integrate(S((firstFunc)*(secondFunc)),x) #okay this is possibl the impossibly difficult part. defining the function won't be easy, i don't think, wo presetting it. 
+	print(latex(volume))
 
 # i don't know if this is necessary, or if i can juts print it
 # print(json.dumps({"volume":volume,"volumeExpression":volumeExpression}))
