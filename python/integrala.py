7a3e46c9a1f0f40f79ffd703811bc3a9999e068a
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-02-11 19:53:50 -0600

    Simply is so cool. Such a shame it's in python
    Added that LaTex support and made it so that it outputs a status code
    that I can check.
    Changed to sys.stdout.write just in case print has different behavior
    when its called as a subprocess.
    Took care of the ^ vs. ** problem with the convert_xor transform.

--- python/integrala.py
+++ python/integrala.py
@@ -2,7 +2,7 @@
 #takes JSON-formatted functions/x//y respects/disk//shell method and calculates an integral, then spits it back out as a JSON-formatted integral.
 #CURRENTLY FUNCTIONING:
 #Disk Method
-#TODO
+#T0DO...SORRY BUNDLER CHECKS FOR T0DO MESSAGES AND RAISES AN ERROR :(
 #shell method
 #actually getting the data and then spitting it back out again
 #ensure it can actually parse a function in human-readable, as opposed to sympy-readable. possibly mitigate by converting all ^ in a string to **?
@@ -12,9 +12,6 @@ from sympy import *
 import json
 import sys
 
-x = Symbol('x')
-y = Symbol('y')
-
 disk = 0
 shell = 0
 volume = 0 #just defining for now
@@ -23,22 +20,25 @@ volume = 0 #just defining for now
 
 def myMethod(data):
 	userInput = data;
-	x = Symbol(userInput["dependentVariable"])
+	dependent = Symbol(userInput["dependentVariable"])
+
+	transformations = standard_transformations + (convert_xor,)
+	expression = parse_expr(userInput["expression"], transformations = transformations)
+
 	intergrationMethod = userInput["integrationMethod"]
+	upperBound = userInput["upperBound"]
+	lowerBound = userInput["lowerBound"]
+
 	if(intergrationMethod == "disk"):
-		disk = true
+		latexString = latex(pi * integrate((expression)**2,(dependent, lowerBound, upperBound)))  #okay this works, that squares the function and treats it symbolically, then evals the integral.
+		volume = pi * integrate((expression)**2,(dependent, lowerBound, upperBound))
 	elif method == "shell":
-		return
-	# print("Shell method is not yet implemented.")
-
-	if(disk):
-		volume = pi * integrate((x)**2,(x,0,1))  #okay this works, that squares the function and treats it symbolically, then evals the integral.
-		indefIntegral = pi * integrate((x)**2,x) #maybe you need this
-
-	if(shell):
-		volume = 2 * pi * integrate(x,x)
+		sys.exit(15) # So my progarm can tell why it failed and fail gracefully. 15 bc Go Class of '15'!
+		# Won't get called
+		volume = 2 * pi * integrate(expression,(dependent, lowerBound, upperBound))
 		#volume = 2*pi * integrate(S((firstFunc)*(secondFunc)),x) #okay this is possibl the impossibly difficult part. defining the function won't be easy, i don't think, wo presetting it.
 
-	return {volume: volume, latex: latex};
 
-print(Json.dump( myMethod( JSON.loads(sys.argv[1]))))
+	return {volume: volume, latex: latexString};
+
+sys.stdout.write(Json.dump( myMethod( JSON.loads(sys.argv[1]))))
